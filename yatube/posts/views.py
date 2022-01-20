2945a1019a8179412539477919cff8608ff362d7
Кирилл Дягилев <k0rb3n.baby@gmail.com>
2022-01-21 00:52:07 +0300

    Разбиваем шаблоны: include, block, extends. Настройка VSCode

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(response):
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_details(response, any_slug):
     return HttpResponse(f'Страница {any_slug}')
